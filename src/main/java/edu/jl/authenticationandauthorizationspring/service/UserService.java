bbdd48d307753ea24cf521ceb4c3156ebde2d823
João Lucas <joaolucasdeoliveiralima2@gmail.com>
2024-11-29 01:41:10 -0300

    feat: add UserService.java

---
+++ src/main/java/edu/jl/authenticationandauthorizationspring/service/UserService.java
@@ -0,0 +1,6 @@
+package edu.jl.authenticationandauthorizationspring.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
+}
