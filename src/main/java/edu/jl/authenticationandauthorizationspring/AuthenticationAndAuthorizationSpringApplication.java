b75a64c913e7f25ff58a3377ed26ea3dd796db5e
João Lucas <joaolucasdeoliveiralima2@gmail.com>
2024-11-29 00:09:12 -0300

    initial commit

---
+++ src/main/java/edu/jl/authenticationandauthorizationspring/AuthenticationAndAuthorizationSpringApplication.java
@@ -0,0 +1,13 @@
+package edu.jl.authenticationandauthorizationspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationAndAuthorizationSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationAndAuthorizationSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/jl/authenticationandauthorizationspring/AuthenticationAndAuthorizationSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.jl.authenticationandauthorizationspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationAndAuthorizationSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
